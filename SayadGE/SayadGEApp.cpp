ec3d00263f4ed2f20a113e97cadd7d33bd5aa91c
Paul Sayad <pauljsayad@gmail.com>
2021-10-30 16:12:11 -0400

    intro to glfw

--- SayadGE/SayadGEApp.cpp
+++ SayadGE/SayadGEApp.cpp
@@ -1,17 +1,28 @@
 #include "pch.h"
 #include "SayadGEApp.h"
 
+#include "GLFW/glfw3.h"
+
 namespace SayadGE
 {
 	// Starts the game 
 	void SayadGEApp::Run()
 	{
 		std::cout << "SayadGE is running..." << std::endl;
+		
+		GLFWwindow* window;
+
+		glfwInit();
+
+		window = glfwCreateWindow(800, 600, "Test", NULL, NULL);
 
 		// Game Loop
 		while (true)
 		{
 			OnUpdate();
+
+			glfwSwapBuffers(window);
+			glfwPollEvents();
 		}
 	}
 
